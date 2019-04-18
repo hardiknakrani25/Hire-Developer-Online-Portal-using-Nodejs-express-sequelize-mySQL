a33342ccdd55adb1e4c651c9ba15d7795b544653
Hardik <hdnakarani@gmail.com>
2019-04-18 17:45:31 +0530

    add query for gigs

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.get("/", (req, res) => {
   res.send("INDEX");
 });
 
+app.use("/gigs", require("./routes/gigs"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
